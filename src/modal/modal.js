e219c90ae098c2a524610e919d22cd28fc0fd34a
Danil Pobirskiy <danilpobiryan@yandex.ru>
2022-05-19 11:33:48 +0800

    refactoring modal.css

---
+++ src/modal/modal.js
@@ -0,0 +1,30 @@
+import React from "react";
+import './modal.css'
+
+export default class Modal extends React.Component {
+    state = {
+        isOpen : false
+     }
+    
+     render() {
+    return (
+      <React.Fragment>
+        <button onClick={() => this.setState({ isOpen: true })}>
+          Open modal
+        </button>
+
+        {this.state.isOpen && (
+          <div className='modal'>
+            <div className='modalBody'>
+              <h1>Modal title</h1>
+              <p>I am awesome modal!</p>
+              <button onClick={() => this.setState({ isOpen: false })}>
+                Close modal
+              </button>
+            </div>
+          </div>
+        )}
+      </React.Fragment>
+    )
+  }
+}
